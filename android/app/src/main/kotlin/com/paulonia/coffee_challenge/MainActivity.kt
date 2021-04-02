038dde66d83482f9d692efefa3fdde3f014a0a26
Juan Leon <jjleoncamilo@gmail.com>
2021-04-01 20:45:40 -0500

    delete unncesary files

--- android/app/src/main/kotlin/com/paulonia/coffee_challenge/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.paulonia.coffee_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/app/modules/home/models/coffee_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class CoffeeModel {
-  String path;
-  String name;
-  double price;
-  CoffeeModel({@required this.path, @required this.name, @required this.price});
-}
